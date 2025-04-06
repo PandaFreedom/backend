cdc25d28ec0aea87768c31316533c914abb4ce57
xiongmao <1821655660@qq.com>
2025-04-06 14:51:45 +0800

    feat(实例化 prisma):"服务端实例"

---
+++ src/db.ts
@@ -0,0 +1,9 @@
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleDestroy {
+  async onModuleDestroy() {
+    await this.$disconnect(); // 确保在模块销毁时断开与数据库的连接
+  }
+}
