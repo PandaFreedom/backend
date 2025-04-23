bd001159f2074b90bf15531c9e6a18feea0aa89c
xiongmao <1821655660@qq.com>
2025-04-23 10:13:39 +0800

    fix:"message change"

--- src/login/loginAuth.dto.ts
+++ src/login/loginAuth.dto.ts
@@ -1,10 +1,10 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginAuthDto {
-  @IsNotEmpty()
-  @IsString({ message: '用户名不能为空' })
+  @IsString()
+  @IsNotEmpty({ message: '用户名不能为空' })
   name: string;
-  @IsNotEmpty()
-  @IsString({ message: '密码不能为空' })
+  @IsString()
+  @IsNotEmpty({ message: '密码不能为空' })
   password: string;
 }
